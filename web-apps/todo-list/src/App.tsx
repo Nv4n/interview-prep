c13084ca42eb6029f9e2ccbd51ef07a51c91f870
Sybatron <vipsister2014@gmail.com>
2026-03-11 16:05:17 +0200

    add todo list app

---
+++ web-apps/todo-list/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+let todos = ["Walk the dog", "Water the plants", "Wash the dishes"];
+
+function App() {
+	const [todo, setTodo] = useState("");
+	const [todoList, setTodoList] = useState([
+		"Walk the dog",
+		"Water the plants",
+		"Wash the dishes",
+	]);
+	function addTodo() {
+		setTodoList([...todoList, todo]);
+		setTodo("");
+	}
+	function deleteTodo(indexToDelete: number) {
+		setTodoList(todoList.filter((_, ind) => ind !== indexToDelete));
+	}
+	return (
+		<div>
+			<h1>Todo List</h1>
+			<div>
+				<input
+					type="text"
+					placeholder="Add your task"
+					value={todo}
+					onChange={(e) => setTodo(e.target.value)}
+				/>
+				<div>
+					<button onClick={() => addTodo()}>Submit</button>
+				</div>
+			</div>
+			<ul>
+				{todoList.map((val, ind) => (
+					<li key={`todo-${ind}`}>
+						<span>{val}</span>
+						<button onClick={() => deleteTodo(ind)}>Delete</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default App;
